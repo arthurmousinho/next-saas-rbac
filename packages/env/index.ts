412f2be8db8169a8d4755408aa98210c544f13c6
arthurmousinho <arthursilvamo296@gmail.com>
2025-03-07 11:54:36 -0300

    Refactor environment variable names for consistency

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,9 +1,9 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from 'zod';
 
-export const env = createEnv({
+export const env = createEnv({ 
     server: {
-        SERVER_PORT: z.coerce.number().default(3333),
+        PORT: z.coerce.number().default(3333),
         DATABASE_URL: z.string().url(),
 
         JWT_SECRET: z.string(),
@@ -17,7 +17,7 @@ export const env = createEnv({
         NEXT_PUBLIC_API_URL: z.string().url(),
     },
     runtimeEnv: {
-        SERVER_PORT: process.env.SERVER_PORT,
+        PORT: process.env.PORT,
         DATABASE_URL: process.env.DATABASE_URL,
         JWT_SECRET: process.env.JWT_SECRET,
         GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
